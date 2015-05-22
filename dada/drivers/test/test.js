7f4d5d443c6d824272e589a16ec4ee30d7a1bc74
Alex P Chen <alexp.chen@raritan.com>
2015-05-22 14:32:45 +0800

    testing

---
+++ dada/drivers/test/test.js
@@ -0,0 +1,19 @@
+
+var Zip = require('node-7z'); 
+
+var myTask = new Zip();
+myTask.extractFull('Floormaps.7z', '.').then(function () {
+      alert('Extracting  Floormaps.7z done in your local machine!');
+});;
+var http = require('http');
+var fs = require('fs');
+
+var file = fs.createWriteStream("node_modules.7z");
+var request = http.get("http://192.168.84.198/dada/node_modules.7z", function(response) {
+  response.pipe(file);
+  myTask.extractFull('node_modules.7z', '.').then(function () {
+      alert('Extracting node_modules.7z done in your local machine!');
+});
+
+	 
+});
